f1086c4ee606a48519bec5441cc39007f3cd4175
brandonc16 <brandoncruz1016@gmail.com>
2024-01-26 16:26:18 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'EvidenceBrandon';
+
+  ngOnInit(): void {
+     // Agregar una clase adicional para iniciar la animación en el mensaje
+     const messageElement = document.querySelector('.message');
+     if (messageElement) {
+       messageElement.classList.add('start-typing');
+     }
+ 
+     // Después de 3500 ms (duración de la animación en mensaje), iniciar la animación en el nombre
+     setTimeout(() => {
+       const nameElement = document.querySelector('.myName');
+       if (nameElement) {
+         nameElement.classList.add('start-typing');
+       }
+     }, 3500);
+  }
 }
